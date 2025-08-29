791b28d6e57b47029a902beb854ea291cb82d00c
MatRouillard <mathis.rouillard@ign.fr>
2025-08-29 17:07:28 +0200

    feat(fullscreen): Ajout d'un bouton pour afficher le plein écran
    Les dialogues ne s'affichent pas en mode plein écran. Pb à résoudre ?
    
    #20

---
+++ src/page/fullScreen/fullScreen.js
@@ -0,0 +1,46 @@
+import { FullScreen } from 'ol/control.js';
+// import Toggle from 'ol-ext/control/Toggle.js';
+// import Bar from 'ol-ext/control/Bar.js';
+import Toggle from '../../control/CustomToggle/CustomToggle.js'
+import Bar from '../../control/CustomBar/CustomBar.js';
+import carte from '../../carte.js';
+
+function onToggle() {
+  fullScreen.button_.click();
+}
+
+let fullScreen = new FullScreen({
+  className: 'fr-hidden',
+  activeClassName: 'fr-hidden',
+  inactiveClassName: 'fr-hidden',
+})
+
+fullScreen.on('enterfullscreen', () => {
+  fullScreenToggle.setActive(true);
+})
+
+fullScreen.on('leavefullscreen', () => {
+  fullScreenToggle.setActive(false);
+})
+
+let fullScreenToggle = new Toggle({
+  className: 'fullscreen-toggle',
+  autoActivate: true,
+  classButton: 'fr-btn fr-btn--tertiary-no-outline ri-fullscreen-line',
+  attributes: {
+    title: "Afficher le plein écran",
+    'aria-label': "Afficher le plein écran",
+  },
+  onToggle: onToggle
+})
+
+// TODO : changer la barre pour inclure le zoom
+let bar = new Bar({
+  className: 'temp-fullscreen-bar',
+  controls: [fullScreenToggle],
+})
+
+carte.addControl('fullScreen-hidden', fullScreen)
+carte.addControl('fullScreen', bar)
+
+bar.setPosition('bottom-right')
