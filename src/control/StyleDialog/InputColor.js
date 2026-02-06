a66d448cd5d07eb4aa1b743e09a4b5e9fcca35e5
viglino <jean-marc.viglino@ign.fr>
2026-02-06 12:12:06 +0100

    feat(StyleForm): Style du colorpicker + accessibility

--- src/control/StyleDialog/InputColor.js
+++ src/control/StyleDialog/InputColor.js
@@ -1,6 +1,7 @@
 import Color from 'ol-ext/util/input/Color.js';
+import getUid from "../../utils/getUid";
 
-import './inputColor.scss';
+import './InputColor.scss';
 
 /**
  * Input de type couleur pour le formulaire de style
@@ -10,11 +11,24 @@ class ColorInput extends Color {
     options = options || {};
     options.position = options.position || 'fixed';
     super(options);
+    // Dispatch change event on color change
     this.input.addEventListener('change', e => this.setColor(e.target.value));
+    // Style des boutons
     this.element.querySelectorAll('button').forEach(btn => {
       btn.className = 'fr-btn fr-btn--tertiary';
     });
+    // Popup accessibility
+    this._elt.popup.id = getUid('color-picker-popup');
+    this.element.id = getUid('color-picker');
+    this.element.ariaLabel = 'Sélecteur de couleur';
+    this.element.ariaExpanded = false;
+    this.element.role = 'button';
+    this.element.setAttribute('aria-controls', this._elt.popup.id);
+    this.on('collapse', e => {
+      this.element.ariaExpanded = e.visible;
+    });
   }
+
 }
 
 export default ColorInput;
