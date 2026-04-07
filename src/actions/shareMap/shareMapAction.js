d64071dbecbbb8ac8889dabf555dc81952773daa
viglino <jean-marc.viglino@ign.fr>
2026-04-07 16:11:33 +0200

    UX Partage de cartes #14

--- src/actions/shareMap/shareMapAction.js
+++ src/actions/shareMap/shareMapAction.js
@@ -1,7 +1,8 @@
 import Action from '../Action.js';
 import content from './shareMap.html?raw';
 import './shareMap.scss';
-
+import carte from '../../carte.js';
+import config from 'mcutils/config/config.js';
 /**
  * @type {import('../../control/Dialog/AbstractDialog.js').default}
  * Dialog utilisé par l'action 
@@ -21,6 +22,16 @@ function onOpen(e) {
   copyBtns.forEach(btn => {
     btn.addEventListener('click', copy)
   });
+  console.log('share map opened');
+  // Enter map info in the dialog
+  const url = config.server + 'carte/' + carte.get('id') + '/' + carte.getTitle();
+  dialog.querySelector('#share-link').value = url;
+  dialog.querySelector('#share-iframe').value = `<iframe
+  width="600" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+  sandbox="allow-forms allow-scripts allow-same-origin"
+  src="` + url + `">
+  allowfullscreen>
+</iframe>`;
 }
 
 function copy(e) {
