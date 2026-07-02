3a6940a7ae7445fdbeed5e653e1507242dc6f3f3
viglino <jean-marc.viglino@ign.fr>
2026-07-02 11:48:55 +0200

    [FIX] infobulles à la couche / gestion des erreurs #129

--- src/actions/editLayerPopup/editLayerPopupAction.js
+++ src/actions/editLayerPopup/editLayerPopupAction.js
@@ -1,6 +1,7 @@
 import Action from "../Action.js";
 import ol_ext_element from "ol-ext/util/element.js";
 import PopupForm from "../../control/StyleDialog/popupForm.js";
+import VectorStyle from "mcutils/layer/VectorStyle.js";
 
 import "./editLayerPopup.scss";
 
@@ -27,8 +28,19 @@ function onOpen(e) {
   }
   this.popupForm.setLayer(editLayerPopupAction.layer);
   const content = this.popupForm.getContent();
-  dialog.getDialogContent().appendChild(content);
-  content.querySelector("input").focus();
+  const layer = editLayerPopupAction.layer
+  console.log(layer);
+  if (layer && (layer instanceof VectorStyle || /WMTS|Statistique|MVT|PBF/.test(layer.get('type'))
+      || (layer.getSource() && layer.getSource().getFeatureInfo && layer.get('queryable'))
+  )) {
+    dialog.getDialogContent().appendChild(content);
+    content.querySelector("input").focus();
+  } else {
+    dialog.getDialogContent().innerHTML = `
+    <div class="fr-alert fr-alert--warning fr-alert--sm">
+      <p>Cette couche n'est pas interrogeable et ne permet pas l'affichage d'infobulles.</p>
+    </div>`
+  }
 }
 
 const editLayerPopupAction = new Action({
